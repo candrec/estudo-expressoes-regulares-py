012ad31308565110e7d0dd1e3ee36798d235aa6c
André Almeida <almeidacandrec@gmail.com>
2022-05-22 21:30:25 -0300

    add forma de reconhecer um número de telefone em uma string

--- is_phone_number.py
+++ is_phone_number.py
@@ -18,7 +18,9 @@ def is_phone_number(text):
             return False
     return True
 
-print('415-555-4242 is a phone number:')
-print(is_phone_number('415-555-4242'))
-print('Moshi moshi is a phone number:')
-print(is_phone_number('Moshi moshi'))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if is_phone_number(chunk):
+        print(f'Phone number found: {chunk}')
+print('Done')
